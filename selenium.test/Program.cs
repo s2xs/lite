edc3d8791d87df9378199a294187ee9c2c29b958
Stanislav Shoshkin <stas_shoshkin@rambler.ru>
2017-07-21 14:42:11 +0300

    первый тест

---
+++ selenium.test/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+/* namespace selenium.test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+*/
+class Google
+{
+
+	static void Main(string[] args)
+	{
+		IWebDriver driver = new ChromeDriver();
+		driver.Navigate().GoToUrl("http://www.google.com/");
+		driver.Quit();
+	}
+
+}
